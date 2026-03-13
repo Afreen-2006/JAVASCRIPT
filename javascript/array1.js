93968991e019d73ca2557d2ba3c914c2981cac9e
Afreen-2006 <dafreen42@gmail.com>
2026-03-13 11:52:12 +0530

    Add files via upload

---
+++ javascript/array1.js
@@ -0,0 +1,13 @@
+let arr=[1,2,4];
+arr.push(4);
+console.log(arr);
+let arr1=[1,4,7,8,2];
+arr1.pop();
+console.log(arr1)
+let arr2=[1,4,7,8,2];
+arr2.shift();
+console.log(arr2);
+let arr3=[1,4,7,8,2];
+arr3.unshift(0);
+console.log(arr3)
+console.log(arr3.length);
